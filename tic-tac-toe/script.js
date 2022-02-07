91f72b63fbb9ad69a0ad0334278c3d06b0f26c99
Hammad Ali <hammad.a22002@gmail.com>
2022-02-07 15:10:07 +0500

    Add tic-tac-toe

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", handler)
+
+function handler(event){
+    var value = "X";
+    var msgDiv = document.querySelector("#message");
+    function buttonHandler(event){
+        //change box value
+        if(!this.value){
+            if(value === "X"){
+                this.value = "X";
+                value = "O";
+                msgDiv.innerText = "Player 1 Turn (X)";
+            }else if(value === "O"){
+                this.value = "O";
+                value = "X";
+                msgDiv.innerText = "Player 2 Turn (O)";
+            }
+        }
+        
+            
+    };
+
+    //buttonHandler.prototype.value = "X";
+
+    //register event handler with all boxes
+    var nodeList = document.querySelectorAll(".box");
+    for(var i=0; i < nodeList.length; i++){
+        nodeList[i].addEventListener("click", buttonHandler);
+    }
+};
